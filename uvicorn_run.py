3738c2105800c984cc8746c6fb6a7f0953548dbc
Yihong Wang <bellowswang@gmail.com>
2023-09-09 13:14:10 +0200

    host the api at https

--- uvicorn_run.py
+++ uvicorn_run.py
@@ -2,4 +2,6 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("modules.api_chatbot:app",
-                host="0.0.0.0", reload=True, port=8080)
+                host="ai-town.mcglobal.ai", reload=True, port=443,
+                ssl_keyfile='/etc/nginx/ssl/ai-town.mcglobal.ai.key',
+                ssl_certfile='/etc/nginx/ssl/ai-town.mcglobal.ai.pem')
