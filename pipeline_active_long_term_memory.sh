ba75540155895dfdf402206920ce4639f2bcf786
Yihong Wang <bellowswang@gmail.com>
2023-11-12 10:20:25 +0100

    [MRD-92] No need to give permission within bash

--- pipeline_active_long_term_memory.sh
+++ pipeline_active_long_term_memory.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-chmod +x "$0"
 /home/ubuntu/memory/.venv/bin/python /home/ubuntu/memory/modules/pipeline_active_long_term_memory.py
--- pipeline_ranking_score.sh
+++ pipeline_ranking_score.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-chmod +x "$0"
 /home/ubuntu/memory/.venv/bin/python /home/ubuntu/memory/modules/pipeline_ranking_score.py
